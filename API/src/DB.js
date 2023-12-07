85faa93573f5e682fd95a75dadb9af75012e3863
JordanZ <jordan.zelayahs@gmail.com>
2023-12-06 20:31:16 -0600

    New DB Connection to MySQL

--- API/src/DB.js
+++ API/src/DB.js
@@ -1,22 +1,19 @@
-const sql = require("mssql");
+const mysql = require('mysql2')
 
-// Configuración de la conexión a la base de datos
-const dbConfig = {
-    user: "jordanZR",
-    password: "UDB123udb",
-    server: "servidorjordan.database.windows.net", // Por ejemplo, "localhost" si está en tu máquina o una dirección IP.
-    database: "asiloAncianos",
-};
-
-// Conectar a la base de datos
-const con = sql.connect(dbConfig, (err) => {
-    if (err) {
-        console.log("Error de conexión a la base de datos:", err);
-    } else {
-        console.log("Conexión exitosa a la base de datos.");
-    }
+var con = mysql.createConnection({
+    host: "localhost",
+    database: "AsiloAncianos",
+    user: "root",
+    password: "1234",
+    connectTimeout: 500000
 });
 
+try{
+    con.connect()
+    console.log("DB Connected")
+}catch(err){
+    throw err
+}
+
+
 module.exports = con
-
-
